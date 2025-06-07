dd7ae92a4d787fc61a1e488983870f481f580697
Zulham Ardiansyah <zulhamardiansyah@yahoo.com>
2025-06-07 10:34:33 -0700

    Update .gitignore & clean git cache

---
+++ components/RainbowKitButton.js
@@ -0,0 +1,16 @@
+// components/RainbowKitButton.js
+'use client';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export function RainbowKitButton() {
+  return (
+    <ConnectButton.Custom>
+      {({ openConnectModal }) => (
+        <button onClick={openConnectModal} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+          Connect Wallet
+        </button>
+      )}
+    </ConnectButton.Custom>
+  );
+}
